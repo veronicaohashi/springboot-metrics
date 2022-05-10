7dc0c9af82192950b5098e31c66ef94e30939960
veronicaohashi <veronica.ohashi@outlook.com>
2022-05-09 22:07:27 -0300

    Initial commit

---
+++ src/main/kotlin/com/veronica/springbootmetrics/SpringbootMetricsApplication.kt
@@ -0,0 +1,11 @@
+package com.veronica.springbootmetrics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootMetricsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootMetricsApplication>(*args)
+}
---
+++ src/test/kotlin/com/veronica/springbootmetrics/SpringbootMetricsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.veronica.springbootmetrics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootMetricsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
